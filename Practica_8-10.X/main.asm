74ed50b60f75bd48bd081df1a4c383b182d79f3e
Alan Franco <alanfrancob97@gmail.com>
2024-02-02 09:44:30 -0600

    Practica_8-01 al 10 modificado
    - Se modificarion la configuración de bits de las prácticas
      Elemental_01.asm al Elemental_10.asm del capitulo 8 del libro para que
      se pueda ejecutar en el PIC16F877A en físico.

--- Practica_8-10.X/main.asm
+++ Practica_8-10.X/main.asm
@@ -2,10 +2,10 @@ LIST P=16F877A
 #include "p16f877a.inc"
 
 ; Configuracion para PicSimLab
-__CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
+; __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
 ; Configuracion para el entrenador en fisico
-; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CP_OFF    
+__CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CP_OFF    
 
     ORG 0			    ; Indica el inicio del programa
 
