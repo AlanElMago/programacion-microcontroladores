f044d9cf7abf3df942b41c250f1ae8b834d74874
Alan Franco <alanfrancob97@gmail.com>
2024-02-04 12:45:31 -0600

    PreubaSalidaPuertoB creado
    Se creó un proyecto de MPLabX para realizar pruebas de salida con el
    puerto B del PIC16F877A.

---
+++ PruebaSalidaPuertoB.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=OutputTestPortB.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/OutputTestPortB.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=outputtestportb.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/outputtestportb.x.tar
