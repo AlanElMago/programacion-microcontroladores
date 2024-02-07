53149c7b43bededb3a7ff0f095ed31627bec26a5
Alan Franco <alanfrancob97@gmail.com>
2024-02-07 17:04:29 -0600

    Practica_8-1 al 10 modificado
    - Se modifcaron los bits de configuración de las prácticas para que el
      PIC16F877A no trabaje en modo de programación de voltaje bajo:
      _LVP_OFF
    - Se agregaron dos instrucciones adicionales que limpian los puertos de
      entrada y salida (PORTD y PORTB) al final de la fase de inicialización
      (SETUP)

--- Practica_8-04.X/main.asm
+++ Practica_8-04.X/main.asm
@@ -5,7 +5,7 @@ LIST P=16F877A
 __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
 ; Configuracion para el entrenador en fisico
-; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CP_OFF   
+; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
     ORG 0			    ; Indica el inicio del programa
 
@@ -15,6 +15,8 @@ SETUP				    ; Etiqueta SETUP
     MOVLW   b'00011111'
     MOVWF   TRISD		    ; Los ultimos 5 pines del puerto D son de entrada
     BCF	    STATUS,	RP0	    ; Cambiar al Banco 0
+    CLRF    PORTB
+    CLRF    PORTD
 
 LOOP				    ; Etiqueta LOOP
     MOVF    PORTD,	0	    ; Leer los pines del puerto D y moverlos al registro W
