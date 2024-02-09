5a165f6b3b26f22af75c4da5a522a37a23407bb8
Alan Franco <alanfrancob97@gmail.com>
2024-02-08 22:01:54 -0600

    Practica_8-01 al 10 y PruebaSalidaPueroB modificado
    - Se arregló el formato del código fuente para que se pudieran trasladar
      fácilmente a un documento Word (.docx).
    - Se arregaron y se modificaron algunos comentarios para facilitar la
      lectura.

--- Practica_8-01.X/main.asm
+++ Practica_8-01.X/main.asm
@@ -7,23 +7,23 @@ __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & 
 ; Configuracion para el entrenador en fisico
 ; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
     
-CONST EQU b'00001101'		    ; Constante CONST es igual a 13
+CONST EQU b'00001101'               ; Constante CONST es igual a 13
 
-    ORG 0			    ; Indica el inicio del programa
+    ORG 0                           ; Indica el inicio del programa
 
-SETUP				    ; Etiqueta SETUP
-    BSF	    STATUS,	RP0	    ; Cambiar al Banco 1
-    CLRF    TRISB		    ; Todos los pines del puerto B son de salida
+SETUP                               ; Etiqueta SETUP
+    BSF     STATUS,     RP0         ; Cambiar al banco 1
+    CLRF    TRISB                   ; Todos los pines del puerto B son de salida
     MOVLW   b'00011111'
-    MOVWF   TRISD		    ; Los ultimos 5 pines del puerto D son de entrada
-    BCF	    STATUS,	RP0	    ; Cambiar al Banco 0
-    CLRF    PORTB
-    CLRF    PORTD
+    MOVWF   TRISD                   ; Los ultimos 5 pines del puerto D son de entrada
+    BCF     STATUS,     RP0         ; Cambiar al banco 0
+    CLRF    PORTB                   ; Limpiar el puerto B
+    CLRF    PORTD                   ; Limpiar el puerto D
 
-LOOP				    ; Etiqueta LOOP
-    MOVF    PORTD,	0	    ; Leer los pines del puerto D y mover el resultado al registro W
-    ADDLW   CONST		    ; Sumar el constante CONST al registro W
-    MOVWF   PORTB		    ; Mover el resultado de la suma (W) al puerto B
-    GOTO    LOOP		    ; Ir a la etiqueta LOOP
-    
+LOOP                                ; Etiqueta LOOP
+    MOVF    PORTD,      0           ; Leer los pines del puerto D y mover el resultado al registro W
+    ADDLW   CONST                   ; Sumar el constante CONST al registro W
+    MOVWF   PORTB                   ; Mover el resultado de la suma (W) al puerto B
+    GOTO    LOOP                    ; Ir a la etiqueta LOOP
+
     END
--- Practica_8-09.X/main.asm
+++ Practica_8-09.X/main.asm
@@ -7,23 +7,23 @@ __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & 
 ; Configuracion para el entrenador en fisico
 ; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
-MASK EQU b'01010101'		    ; Mascara para invertir los bits pares con la operacion XOR
+MASK EQU b'01010101'                ; Mascara para invertir los bits pares con la operacion XOR
 
-    ORG 0
+    ORG 0                           ; Indica el inicio del programa
 
-SETUP
-    BSF	    STATUS,	RP0	    ; Cambiar al banco 1
-    CLRF    TRISB		    ; Todos los pines del puerto B son de salida
+SETUP                               ; Etiqueta SETUP
+    BSF     STATUS,     RP0         ; Cambiar al banco 1
+    CLRF    TRISB                   ; Todos los pines del puerto B son de salida
     MOVLW   b'00011111'
-    MOVWF   TRISD		    ; Los ultimos 5 pines del puerto D son de entrada
-    BCF	    STATUS,	RP0	    ; Cambiar al banco 0
-    CLRF    PORTB
-    CLRF    PORTD
+    MOVWF   TRISD                   ; Los ultimos 5 pines del puerto D son de entrada
+    BCF     STATUS,     RP0         ; Cambiar al banco 0
+    CLRF    PORTB                   ; Limpiar el puerto B
+    CLRF    PORTD                   ; Limpiar el puerto D
 
-LOOP
-    MOVF    PORTD,	0	    ; Leer los pines del puerto D y mover el resultado al registro W
-    XORLW   MASK		    ; Operacion XOR del constante MASK con el registro W
-    MOVWF   PORTB		    ; Mover el resultado de la operacion (W) al puerto B
-    GOTO    LOOP		    ; Ir a la etiqeta LOOP
+LOOP                                ; Etiqueta LOOP
+    MOVF    PORTD,      0           ; Leer los pines del puerto D y mover el resultado al registro W
+    XORLW   MASK                    ; Operacion XOR del constante MASK con el registro W
+    MOVWF   PORTB                   ; Mover el resultado de la operacion (W) al puerto B
+    GOTO    LOOP                    ; Ir a la etiqeta LOOP
 
     END
