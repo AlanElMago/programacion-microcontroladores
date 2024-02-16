a4c65d315f57f59cdc40b79690a8a997ee356be6
Alan Franco <alanfrancob97@gmail.com>
2024-02-16 10:28:58 -0600

    Saltos_05 modificado
    - Se arregló el formato del código fuente.
    - Se cambió la configuración de los bits para el PicSimLab

--- Saltos_05.X/main.asm
+++ Saltos_05.X/main.asm
@@ -2,14 +2,14 @@ LIST P=16F877A
 #include "p16f877a.inc"
 
 ; Configuracion para PicSimLab
-; __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
+__CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
 ; Configuracion para el entrenador en fisico
-__CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
+; __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_ON & _BOREN_ON & _LVP_OFF & _CPD_OFF & _WRT_OFF & _CP_OFF
 
-CONST EQU b'00001101'       ; Constante CONST es igual a 13
+CONST EQU b'00001101'               ; Constante CONST es igual a 13
     
-    ORG 0                   ; Indica el inicio del programa
+    ORG 0                           ; Indica el inicio del programa
 
 SETUP
     BSF     STATUS,     RP0
@@ -23,13 +23,12 @@ SETUP
 MAIN
     MOVLW   CONST
     SUBWF   PORTD,      W
-    MOVLW   b'11110000'     ; Supone que (PORTD) es menor. 
-    BTFSC   STATUS,     C   ; ¿C=0?, ¿(W) negativo?, ¿(PORTD) < CONST? 
-    MOVLW   b'01010101'     ; Supone que (PORTD) es mayor. 
-    BTFSC   STATUS,     Z   ; ¿Z=0?, ¿son distintos? 
-    MOVLW   b'11111111'     ; No, por tanto (PORTD) = NUMERO. 
-    MOVWF   PORTB           ; Resultado se visualiza por el puerto de salida. 
-    GOTO    MAIN            ; Crea un bucle cerrado e infinito.
-    
+    MOVLW   b'11110000'             ; Supone que (PORTD) es menor. 
+    BTFSC   STATUS,     C           ; Â¿C=0?, Â¿(W) negativo?, Â¿(PORTD) < CONST? 
+    MOVLW   b'01010101'             ; Supone que (PORTD) es mayor. 
+    BTFSC   STATUS,     Z           ; Â¿Z=0?, Â¿son distintos? 
+    MOVLW   b'11111111'             ; No, por tanto (PORTD) = NUMERO. 
+    MOVWF   PORTB                   ; Resultado se visualiza por el puerto de salida. 
+    GOTO    MAIN                    ; Crea un bucle cerrado e infinito.
+
     END
-    
